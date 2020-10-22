1b35177417c62ecb91e157897f92357338fa974d
Alekseev Aleksey <alekseev.a@hotmail.com>
2020-10-22 14:55:51 +0300

    2-1 коммит

--- src/redux/DialogPageReducer.js
+++ src/redux/DialogPageReducer.js
@@ -3,19 +3,19 @@ const ADD_MESSENG='ADD-MESSENG';
 let initionalState = {
     dialogs:[
         {id:1,name:"Алексей Андреевич"},
-        {id:2,name:"Яночка"},
-        {id:3,name:"Татарин"},
-        {id:4,name:"Норм пацан"},
+        {id:2,name:"User 2"},
+        {id:3,name:"User 3"},
+        {id:4,name:"User 4"},
         {id:5,name:"User 5"},
         {id:6,name:"User 6"}
     ],
     messenges :[
-        {id:1,messenges:"В пизду эту вёрстку"},
-        {id:2,messenges:"Проебал 2 часа на какую то хуиту"},
-        {id:3,messenges:"Работает отлично"},
-        {id:4,messenges:"Тест сообщения 4"},
-        {id:5,messenges:"тест 5"},
-        {id:6,messenges:"Почему бы не 6"},
+        {id:1,messenges:"Messenges 1"},
+        {id:2,messenges:"Messenges 2"},
+        {id:3,messenges:"Messenges 3"},
+        {id:4,messenges:"Messenges 4"},
+        {id:5,messenges:"Messenges 5"},
+        {id:6,messenges:"Messenges 6"},
       ]
 };
 const dialogPageReducer=(state=initionalState,action)=> {
